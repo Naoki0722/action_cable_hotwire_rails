f3bfd007efea4f9e6dc0dafba437b88049546cb6
Naoki0722 <mattu.nao722@gmail.com>
2024-10-19 15:49:21 +0900

    Refactor home controller test to use root path instead of deprecated home_index_url

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   test "should get index" do
     get root_path
     assert_response :success
+  end
+
+  test "authed redirect index" do
+    sign_in users(:one)
+    get new_user_session_path
+    assert_response :redirect
+    assert_redirected_to root_path
   end
 end
