8a41328737a481dd7a66934f543feaeeac642da7
Naoki0722 <mattu.nao722@gmail.com>
2024-10-23 07:35:33 +0900

    Refactor MessagesController and views, and add new message creation functionality

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,9 +2,18 @@
 
 class MessagesController < ApplicationController
   def new
+    p params[:room_id]
     @message = Message.new
   end
 
   def create
+    p params[:room_id]
+    p create_params
+  end
+
+  private
+
+  def create_params
+    params.require(:message).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  resources :rooms, only: %i[index show new create]
-  resources :messages, only: %i[new create]
+  resources :rooms, only: %i[index show new create] do
+    resources :messages, only: %i[new create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
