e9cf2632dd33e26a16ef89dca7eb87de64b96971
Naoki0722 <mattu.nao722@gmail.com>
2024-10-28 23:16:17 +0900

    Refactor MessagesController and views, and add new message creation functionality

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,18 +2,25 @@
 
 class MessagesController < ApplicationController
   def new
-    p params[:room_id]
-    @message = Message.new
+    @room = Room.find(params[:room_id])
+    @message = Message.new(room: @room, user: current_user)
   end
 
   def create
-    p params[:room_id]
-    p create_params
+    p create_params[:user_id]
+    message = Message.new(create_params)
+    if message.save
+      flash[:success] = "Message sent!"
+      redirect_to room_path(message.room)
+    else
+      flash[:error] = message.errors.full_messages.join(", ")
+      redirect_to new_room_message_path(message.room)
+    end
   end
 
   private
 
   def create_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :room_id, :user_id)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -7,26 +7,19 @@ class Room < ApplicationRecord
 
   validates :creator_id, uniqueness: { scope: :recipient_id }
 
-  before_validation :normalize_user_order
 
   scope :own_room, ->(user_id) { where("creator_id = ? OR recipient_id = ?", user_id, user_id) }
 
   def recipient_for(user)
     if creator?(user)
-      creator
+      return recipient
     end
-    recipient
+    creator
   end
 
   private
 
   def creator?(user)
     user == creator
-  end
-
-  def normalize_user_order
-    if creator_id > recipient_id
-      self.creator_id, self.recipient_id = recipient_id, creator_id
-    end
   end
 end
